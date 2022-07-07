f91c0d1e635b1c665a4b7f67987a9cd5276921a1
Artur Bartkowicz <artur_bartkowicz@epam.com>
2022-07-07 17:04:14 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import NewReminder from './components/NewReminder';
+import ReminderList from './components/ReminderList';
+import Reminder from './interfaces/Reminder';
+import reminderService from './services/reminder';
 
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([])
+
+  useEffect(() => {
+    (async () => {
+      const reminders = await reminderService.getReminders()
+      setReminders(reminders)
+    })()
+  }, [])
+
+  const removeReminder = (id: number) => {
+    setReminders(reminders.filter(reminder => reminder.id !== id))
+  }
+
+  const addReminder = async (title: string) => {
+    const newRreminder = await reminderService.addReminder(title)
+    setReminders([newRreminder, ...reminders])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewReminder onAddReminder={addReminder}/>
+      <ReminderList items={reminders} onRemoveReminder={removeReminder}/>
     </div>
   );
 }
---
+++ src/components/ReminderList.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Reminder from '../interfaces/Reminder';
+
+interface RemindersListProps {
+  items: Reminder[]
+  onRemoveReminder: (id: number) => void
+}
+
+function ReminderList({ items, onRemoveReminder }: RemindersListProps) {
+  return (
+   <ul className='list-group'>
+		{items.map(item => 
+      <li className='list-group-item' key={item.id}>
+        {item.title}
+        <button className='btn btn-outline-danger mx-2 rounded-pill' onClick={() => onRemoveReminder(item.id)}>Delete</button>
+      </li>)}
+	 </ul>
+  );
+}
+
+export default ReminderList;
